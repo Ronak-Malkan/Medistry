9dc90f53cca8d5f170bb76e94c43e5bf98a719c6
RonakMalkan <ronakmalkan2062001@gmail.com>
2025-06-28 11:31:45 +0530

    chore: ignore dist folders in gitignore and remove existing backend/dist from tracking

--- backend/dist/cron/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerCronJobs = registerCronJobs;
-//import cron from 'node-cron';
-const logger_1 = require("../utils/logger");
-function registerCronJobs() {
-    // TODO: schedule expiry cleanup, low-stock, etc.
-    logger_1.logger.info('Cron jobs registration stub');
-}
--- backend/dist/repositories/accountRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.accountRepository = void 0;
-const Account_1 = require("../entities/Account");
-const data_source_1 = require("../data-source");
-exports.accountRepository = data_source_1.AppDataSource.getRepository(Account_1.Account);
